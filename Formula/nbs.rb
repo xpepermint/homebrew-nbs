d78285ae0c4fd972bc18a4ce6c3aa42ab4cda6e9
Kristijan Sedlak <xpepermint@gmail.com>
2020-06-06 12:18:47 +0200

    x

--- Formula/nbs.rb
+++ Formula/nbs.rb
@@ -4,7 +4,7 @@ class Nbs < Formula
   version "0.0.1"
 
   if OS.mac?
-    url "https://github.com/xpepermint/nbs/archive/0.0.2.tar.gz"
+    url "https://github.com/xpepermint/nbs/archive/0.0.3.tar.gz"
     sha256 "d28a31f513c83c5355e2d91d6d5b5cc441f45fe24f9d62a982ba3a680a61b5ab"
   end
 
